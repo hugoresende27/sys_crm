6e9927c8102b5fad2dcf36c3cf0963e8db3ff2b3
hugoresende <hugo.resende@iservices.pt>
2023-12-17 11:17:58 +0000

    sys_crm first

---
+++ README.md
@@ -0,0 +1,2 @@
+### setup guide
+- (link)[https://www.youtube.com/watch?v=3Hg2WPwDyG8&ab_channel=ZakH.]
---
+++ app/settings.php
@@ -0,0 +1,10 @@
+<?php
+use Psr\Container\ContainerInterface;
+
+return function (ContainerInterface $container) {
+    $container->set('settings', function() {
+        return [
+
+        ];
+    });
+};
